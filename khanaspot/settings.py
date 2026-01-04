f708ad276bb26c0667fdce227a479bce06043e67
Anil Chaudhary <chaudhary98.sc@gmail.com>
2026-01-04 19:58:16 +0545

    homepage setup,postgresql config

--- khanaspot/settings.py
+++ khanaspot/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gbj3(6tg3!um*b2xq(k1**!(@80s#-hxkvmr#74wmq!#grsy=k'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG',cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -74,8 +75,11 @@ WSGI_APPLICATION = 'khanaspot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':config('DB_NAME'),
+        'USER':config('DB_USER'),
+        'PASSWORD':config('DB_PASSWORD'),
+        'HOST':config('DB_HOST'),
     }
 }
 
@@ -117,5 +121,5 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT=BASE_DIR/'static'
 STATICFILES_DIRS=[
-    'khanaspot'
+    'khanaspot/static'
 ]
